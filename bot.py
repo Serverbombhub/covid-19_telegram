b90d582ff18204f2365c04afe0afb8fe4739745e
beloslav <velislav1329@gmail.com>
2020-03-28 11:44:13 +0300

    сделал чтобы запрос по АПИ был по требованию.

--- bot.py
+++ bot.py
@@ -10,40 +10,10 @@ headers = {
     'x-rapidapi-key': "87c228d61bmsh9d6557cc14b79e5p156549jsn8a1e39e3dffa"
 }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-json_data = json.loads(response.text)
-data = json_data['data']['covid19Stats']
-last_update = json_data['data']['covid19Stats'][0]['lastUpdate']
-stats = {}
-
-for x in data:
-    if 'confirmed' in stats:
-        stats['confirmed'] += x['confirmed']
-    else:
-        stats['confirmed'] = x['confirmed']
-    if 'deaths' in stats:
-        stats['deaths'] += x['deaths']
-    else:
-        stats['deaths'] = x['deaths']
-    if 'recovered' in stats:
-        stats['recovered'] += x['recovered']
-    else:
-        stats['recovered'] = x['recovered']
-
-confirmed, deaths, recovered = stats.values()
-
-
 bot = telebot.TeleBot(token)
 message_start_help = "Привет, {}! Тут ты можешь узнать полную статистику о COVID-19. " \
                      "Напиши 'хочу знать' и я выдам тебе статистику."
 
-message_stat = f'Окей, вот статистика по всему миру(данные могуть быть с задержкой в несколько часов):\n' \
-               f'Дата обновления статистики: {last_update}\n' \
-               f'Подтверждённых случаев: {confirmed}\n' \
-               f'Выздоровевших: {recovered}\n' \
-               f'Смертей: {deaths}\n\n' \
-               f'Карта с официальной статистикой:' \
-               f'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6'
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -54,6 +24,36 @@ def send_welcome(message):
 @bot.message_handler(content_types=['text'])
 def reply_to_user(message):
     if message.text.lower() == 'хочу знать':
+        response = requests.request("GET", url, headers=headers)
+        json_data = json.loads(response.text)
+        data = json_data['data']['covid19Stats']
+        # last_update = json_data['data']['covid19Stats'][0]['lastUpdate']
+        stats = {}
+        last_check = json_data['data']['lastChecked']
+        last_check = last_check[0:10] + ' ' + last_check[11:19]
+        for x in data:
+            if 'confirmed' in stats:
+                stats['confirmed'] += x['confirmed']
+            else:
+                stats['confirmed'] = x['confirmed']
+            if 'deaths' in stats:
+                stats['deaths'] += x['deaths']
+            else:
+                stats['deaths'] = x['deaths']
+            if 'recovered' in stats:
+                stats['recovered'] += x['recovered']
+            else:
+                stats['recovered'] = x['recovered']
+
+        confirmed, deaths, recovered = stats.values()
+
+        message_stat = f'Окей, вот статистика по всему миру(данные могут быть с задержкой в несколько часов):\n' \
+                       f'Дата обновления статистики: {last_check}\n' \
+                       f'Подтверждённых случаев: {confirmed}\n' \
+                       f'Выздоровевших: {recovered}\n' \
+                       f'Смертей: {deaths}\n\n' \
+                       f'Карта с официальной статистикой: ' \
+                       f'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6'
         bot.reply_to(message, message_stat)
 
     elif message.text.lower() == 'помощь':
@@ -69,4 +69,4 @@ def reply_to_user(message):
 
 
 if __name__ == '__main__':
-    bot.polling()
+    bot.polling(none_stop=True)
