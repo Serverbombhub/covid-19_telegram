852b6c6eb099960df44441ae2ff0a8fcf7027e7d
beloslav <velislav1329@gmail.com>
2020-04-01 11:54:19 +0300

    добавил выделения статистики, закомментировал лог

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ fh.addHandler(main_fh)
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(selective=True)
     name_user = message.json['from']['first_name']
-    fh.info(f'Поступил запрос команды {message.text} от {name_user} id {message.json["from"]["id"]}')
+    # fh.info(f'Поступил запрос команды {message.text} от {name_user} id {message.json["from"]["id"]}')
     markup.row(get_stat_user, help_user)
     markup.row(map_covid)
     bot.send_message(message.chat.id, message_start_help.format(name_user), reply_markup=markup)
@@ -41,7 +41,7 @@ def send_welcome(message):
 @bot.message_handler(content_types=['text'])
 def reply_to_user(message):
     if message.text.lower() == 'хочу знать':
-        fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
+        # fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
         response = requests.request("GET", url, headers=headers)
         json_data = json.loads(response.text)
         data = json_data['data']['covid19Stats']
@@ -66,16 +66,16 @@ def reply_to_user(message):
         confirmed, deaths, recovered = stats.values()
 
         message_stat = f'Окей, вот статистика по всему миру(данные могут быть с задержкой в несколько часов):\n' \
-                       f'Дата обновления статистики: {last_check}\n' \
-                       f'Подтверждённых случаев: {confirmed}\n' \
-                       f'Выздоровевших: {recovered}\n' \
-                       f'Смертей: {deaths}\n\n' \
+                       f'Дата обновления статистики: <b>{last_check}</b>\n' \
+                       f'Подтверждённых случаев: <b>{confirmed}</b>\n' \
+                       f'Выздоровевших: <b>{recovered}</b>\n' \
+                       f'Смертей: <b>{deaths}</b>\n\n' \
                        f'Карта с официальной статистикой: ' \
                        f'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6'
-        bot.reply_to(message, message_stat)
+        bot.reply_to(message, message_stat, parse_mode='html')
 
     elif message.text.lower() == 'помощь':
-        fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
+        # fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
         markup = types.ReplyKeyboardMarkup(selective=True)
         fuck_bot = types.KeyboardButton('просто отвали')
         markup.row(get_stat_user, map_covid)
@@ -85,23 +85,23 @@ def reply_to_user(message):
                                           "Либо можно просто получить ссылку на официальную карту.", reply_markup=markup)
 
     elif message.text.lower() == 'привет' or message.text.lower() == 'привет!':
-        fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
+        # fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
         markup = types.ReplyKeyboardMarkup(selective=True)
         markup.row(get_stat_user, help_user)
         name_user = message.json['from']['first_name']
         bot.send_message(message.chat.id, message_start_help.format(name_user), reply_markup=markup)
 
     elif message.text.lower() == 'просто отвали':
-        fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
+        # fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
         bot.reply_to(message, 'Ну и ладно, мне как-то всё равно.')
 
     elif message.text.lower() == 'хочу карту':
-        fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
+        # fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
         bot.reply_to(message, 'Держи: '
                               'https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6')
 
     else:
-        fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
+        # fh.info(f'Поступил запрос команды "{message.text}" от {message.json["from"]["first_name"]} id {message.json["from"]["id"]}')
         markup = types.ReplyKeyboardMarkup(selective=True)
         markup.row(get_stat_user, help_user)
         bot.send_message(message.chat.id, 'Не понимаю, нажми "хочу знать" либо "помощь"', reply_markup=markup)
